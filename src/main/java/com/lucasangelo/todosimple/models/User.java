49455bb30f4cbbfe5ef24171ec74c137ea3b2ed4
JoseAugustoDosSantos <augusto03jose@gmail.com>
2024-07-20 16:40:17 -0300

    Users model

---
+++ src/main/java/com/lucasangelo/todosimple/models/User.java
@@ -0,0 +1,145 @@
+package com.lucasangelo.todosimple.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+    public interface CreateUser {
+    }
+
+    public interface UpdateUser {
+    }
+
+    public interface DeleteUser {
+    }
+
+    public static final String TABLE_NAME = "users";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String username;
+
+    @Column(name = "email", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 100)
+    private String email;
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull(groups = { CreateUser.class, UpdateUser.class })
+    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
+    @Size(groups = { CreateUser.class, UpdateUser.class }, min = 2, max = 60)
+    private String password;
+
+    @Column(name = "balance", length = 60, nullable = false)
+    @NotNull(groups = { CreateUser.class, UpdateUser.class })
+    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
+    @Size(groups = { CreateUser.class, UpdateUser.class }, min = 1, max = 60)
+    private Double balance;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String email, String password, Double balance) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null)
+            return false;
+
+        if (!(obj instanceof User))
+            return false;
+
+        User other = (User) obj;
+        if (this.id == null)
+            if (other.id != null)
+                return false;
+            else if (!this.id.equals(other.id))
+                return false;
+
+        return Objects.equals(this.id, other.id) &&
+                Objects.equals(this.username, other.username) &&
+                Objects.equals(this.email, other.email) &&
+                Objects.equals(this.password, other.password) &&
+                Objects.equals(this.balance, other.balance);
+
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+}
